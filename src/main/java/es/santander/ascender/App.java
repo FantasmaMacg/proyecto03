c917463c65e477e3bf6223b7eff9f18c227a2b29
MACG <curso08@nomail.com>
2024-11-05 11:56:36 +0100

    proyecto limpio

---
+++ src/main/java/es/santander/ascender/App.java
@@ -0,0 +1,13 @@
+package es.santander.ascender;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
